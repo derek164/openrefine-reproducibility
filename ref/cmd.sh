27e4de95af11f35a23880214663b53fcf4e389c5
Derek Zhang <derek.zhang@C02FG1ZMQ05N-Maven-Wave.local>
2023-07-04 15:33:23 +0800

    push image to dockerhub

--- ref/cmd.sh
+++ ref/cmd.sh
@@ -3,3 +3,8 @@ docker attach openrefine-client
 docker-compose run --rm -it --entrypoint python openrefine-client /app/main.py
 # docker-compose run --rm -it --entrypoint openrefine openrefine-client --list
 # docker-compose run --rm -it --entrypoint openrefine openrefine-client --info "wine"
+
+
+docker build --no-cache -t openrefine-python-client:latest .
+docker tag openrefine-python-client:latest derekz325/openrefine-python-client:latest
+docker push derekz325/openrefine-python-client:latest
