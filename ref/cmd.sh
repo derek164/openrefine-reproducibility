305713b9fc30c153a01b85490e31f67a1246abcd
Derek Zhang <derek.zhang@C02FG1ZMQ05N-Maven-Wave.local>
2023-07-06 09:00:03 +0800

    build multi-arch

--- ref/cmd.sh
+++ ref/cmd.sh
@@ -1,11 +1,12 @@
 docker compose up
 docker attach openrefine-client
-docker-compose run --rm -it --entrypoint python openrefine-client /app/main.py
-docker-compose run --rm -it --entrypoint openrefine openrefine-client --list
-docker-compose run --rm -it --entrypoint openrefine openrefine-client --info "wine"
+docker-compose run --rm --entrypoint python openrefine-client /app/main.py
+docker-compose run --rm --entrypoint openrefine openrefine-client --list
+docker-compose run --rm --entrypoint openrefine openrefine-client --info "wine"
 
 
-docker build --no-cache -t openrefine-python-client:latest .
+# docker build --no-cache -t openrefine-python-client:latest .
+docker buildx build --no-cache --platform linux/amd64 -f ref/Dockerfile -t openrefine-python-client:latest .
 docker tag openrefine-python-client:latest derekz325/openrefine-python-client:latest
 docker push derekz325/openrefine-python-client:latest
 
