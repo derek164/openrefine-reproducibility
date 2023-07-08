fe8eb35d4db65f55860f375790c89c8a62e3e81f
Derek Zhang <derek.zhang@C02FG1ZMQ05N-Maven-Wave.local>
2023-07-08 17:18:45 +0800

    evidence

--- app/explore.py
+++ app/explore.py
@@ -1,6 +1,8 @@
 import duckdb
+import numpy as np
 import pandas as pd
 from pathlib import Path
+from scipy.stats import shapiro
 
 root = Path(__file__).parent
 wine_dirty = pd.read_csv(root / "data" / "wine_dirty.csv")
@@ -42,6 +44,7 @@ results = conn.execute(
         AND variety IS NOT NULL
     GROUP BY country
     ORDER BY COUNT(DISTINCT variety) DESC
+    LIMIT 10
 """
 ).df()
 print(results.to_string())
@@ -63,15 +66,58 @@ print(results.to_string())
 
 results = conn.execute(
     """
-    SELECT sum(case when sample_size >= 30 then 1 else 0 end) as region_clt_valid
-        , sum(case when sample_size < 30 then 1 else 0 end) as region_clt_invalid
+    SELECT title
+        , AVG(points) AS sample_mean
+    FROM wine_clean
+    WHERE title IS NOT NULL
+    GROUP BY title
+    ORDER BY AVG(points) DESC
+    LIMIT 10
+"""
+).df()
+print(results.to_string())
+
+results = conn.execute(
+    """
+    SELECT SUM(CASE WHEN sample_size >= 30 THEN 1 ELSE 0 END) AS region_clt_valid
+        , SUM(CASE WHEN sample_size < 30 THEN 1 ELSE 0 END) AS region_clt_invalid
     FROM (
-        SELECT region_1 as region
-            , count(*) as sample_size
+        SELECT region_1 AS region
+            , COUNT(*) AS sample_size
         FROM wine_clean
         GROUP BY region_1
-        ORDER BY count(*) DESC
+        ORDER BY COUNT(*) DESC
     )
 """
 ).df()
 print(results.to_string())
+
+df = conn.execute(
+    """
+    SELECT points, region_1 as region
+    FROM wine_clean
+    WHERE region_1 in 
+    (
+        SELECT region_1
+        FROM wine_clean
+        GROUP BY region_1
+        HAVING COUNT(*) >= 30
+    )
+    ORDER BY region_1
+"""
+).df()
+
+df = (
+    df.groupby("region")
+    .apply(lambda x: pd.Series(shapiro(x["points"]), index=["stat", "p-value"]))
+    .reset_index()
+    .sort_values("p-value", ascending=False)
+)
+df["normal"] = df["p-value"] > 0.05
+print(df.head(10).to_string())
+
+true_count = df["normal"].sum()
+false_count = len(df) - true_count
+total_count = true_count + false_count
+print(f"true: {true_count, round(true_count / total_count * 100, 2)}")
+print(f"false: {false_count, round(false_count / total_count * 100, 2)}")
